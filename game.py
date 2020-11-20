015634755a6cafbd1f5c85c23f6219cc88542389
unknown <nikolaev.serguei777@gmail.com>
2020-11-20 15:40:19 +0100

    in set_case_def remplace all arg by laby and hero, stop the move of hero when wall

--- game.py
+++ game.py
@@ -22,42 +22,37 @@ def init_game_structure(laby, heroe):
         pygame.display.update()
 
 
-def set_case_definition(walls_coord,
-                        floors_coord,
-                        hero_coord,
-                        guard_coord,
-                        ether_coord,
-                        plastic_tube_coord,
-                        syringe_coord,
-):
+def set_case_definition(laby, heroe):
 
     # fonction d'attribution de cases
 
     show_departure = pygame.image.load(constants.IMAGE_GYVER).convert_alpha()
-    SCREEN.blit(show_departure, hero_coord)
+    SCREEN.blit(show_departure, heroe.heroes_position)
 
     show_guard = pygame.image.load(constants.IMAGE_GUARD).convert_alpha()
-    SCREEN.blit(show_guard, guard_coord)
+    SCREEN.blit(show_guard, laby.position_guard_coord)
 
     show_wall = pygame.image.load(constants.IMAGE_WALL).convert_alpha()
     show_floor = pygame.image.load(constants.IMAGE_FLOOR).convert_alpha()
 
     show_ether_coord = pygame.image.load(constants.IMAGE_ETHER).convert_alpha()
-    SCREEN.blit(show_ether_coord, ether_coord)
+    SCREEN.blit(show_ether_coord, laby.ether_coord)
 
     show_plastic_tube_coord = pygame.image.load(constants.IMAGE_PLASTIC_TUBE).convert_alpha()
-    SCREEN.blit(show_plastic_tube_coord, plastic_tube_coord)
+    SCREEN.blit(show_plastic_tube_coord, laby.plastic_tube_coord)
 
     show_syringe_coord = pygame.image.load(constants.IMAGE_SYRINGE).convert_alpha()
-    SCREEN.blit(show_syringe_coord, syringe_coord)
+    SCREEN.blit(show_syringe_coord, laby.syringe_coord)
 
     # Dimension prémier tuple est la position et le deuxiéme size
-    for coord in walls_coord:
+    for coord in laby.list_walls_coord:
         SCREEN.blit(show_wall, coord)
-    for coord in floors_coord:
+    for coord in laby.list_floors_coord:
         SCREEN.blit(show_floor, coord)
 
+
 def manag_heroes_move(event, laby, heroe):
+    coord_hero_T1 = list(heroe.heroes_position)
     if event.key == pygame.K_UP:
         print("Player moved up!")
     elif event.key == pygame.K_LEFT:
@@ -66,23 +61,17 @@ def manag_heroes_move(event, laby, heroe):
         print("Player moved down!")
     elif event.key == pygame.K_RIGHT:
         # Modifier le tuple pour avoir la futur position
-        coord_hero_T1 = list(heroe.heroes_position)
         coord_hero_T1[0] = coord_hero_T1[0] + 45
-        laby.update_list_floors_coord([heroe.heroes_position, tuple(coord_hero_T1)])
-        print(heroe.heroes_position)
-        print(tuple(coord_hero_T1))
-        heroe.heroes_position = tuple(coord_hero_T1)
-        print("Player moved right!")
-    set_case_definition(
-        laby.list_walls_coord,
-        laby.list_floors_coord,
-        heroe.heroes_position,
-        laby.position_guard_coord,
-        laby.ether_coord,
-        laby.plastic_tube_coord,
-        laby.syringe_coord,
-    )
 
+    if tuple(coord_hero_T1) not in laby.list_walls_coord:
+        laby.update_list_floors_coord([heroe.heroes_position, tuple(coord_hero_T1)])
+        heroe.heroes_position = tuple(coord_hero_T1)
+        set_case_definition(laby, heroe)
+
+
+# # # # # # # # #
+#     MAIN
+# # # # # # # # #
 # Initialisation du Pygame
 pygame.init()
 
@@ -102,15 +91,8 @@ SCREEN.fill(constants.BACKGROUND_COLOUR)
 laby = Maze()
 heroe = Heroes(laby)
 
-set_case_definition(
-    laby.list_walls_coord,
-    laby.list_floors_coord,
-    heroe.heroes_position,
-    laby.position_guard_coord,
-    laby.ether_coord,
-    laby.plastic_tube_coord,
-    laby.syringe_coord,
-)
+set_case_definition(laby, heroe)
+
 
 init_game_structure(laby, heroe)
 
