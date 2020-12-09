ef80f819d2305e825bb781cb688ddde12137a1eb
SergueiNK <nikolaev.serguei777@gmail.com>
2020-12-09 14:02:39 +0100

    test aprés le revert

--- game.py
+++ game.py
@@ -1,26 +1,12 @@
 import time
+
 import pygame
 import constants
 from heroes import Heroes
 from maze import Maze
 
-# définir comme fichier principal avec l'ouverture possible en tant que module(selon le cours)
-# def main():
-#     pass
-#
-# if __name__  ==  "game":
-#     main()
 
-def init_game_structure(
-    list_walls_coord,
-    list_floors_coord,
-    position_guard_coord,
-    ether_coord,
-    plastic_tube_coord,
-    syringe_coord,
-    update_list_floors_coord,
-    heroe,
-    close_game):
+def init_game_structure(laby, heroe, close_game):
     # lancer les modules pygame
     successes, failures = pygame.init()
     print("{0} successes and {1} failures".format(successes, failures))
@@ -47,28 +33,12 @@ def init_game_structure(
                 #si non si:
                 # condition pour la lecture d'appuye sur les touches
                 elif event.type == pygame.KEYDOWN:
-                    manag_heroes_move(event,
-                                      list_walls_coord,
-                                      list_floors_coord,
-                                      position_guard_coord,
-                                      ether_coord,
-                                      plastic_tube_coord,
-                                      syringe_coord,
-                                      update_list_floors_coord,
-                                      heroe
-                    )
+                    manag_heroes_move(event, laby, heroe)
                 # mise à jour de l'écran
                 pygame.display.update()
 
 
-def set_case_definition(
-    list_walls_coord,
-    list_floors_coord,
-    position_guard_coord,
-    ether_coord,
-    plastic_tube_coord,
-    syringe_coord,
-    heroe):
+def set_case_definition(laby, heroe):
 
     # affichage d'image par case
 
@@ -76,41 +46,33 @@ def set_case_definition(
     SCREEN.blit(show_departure, heroe.heroes_position)
 
     show_guard = pygame.image.load(constants.IMAGE_GUARD).convert_alpha()
-    SCREEN.blit(show_guard, position_guard_coord)
+    SCREEN.blit(show_guard, laby.position_guard_coord)
 
     show_wall = pygame.image.load(constants.IMAGE_WALL).convert_alpha()
     show_floor = pygame.image.load(constants.IMAGE_FLOOR).convert_alpha()
 
     # condition d'affichage des objets
     # si les coordonnées de laby.ether_coord ne sont pas nuls alors. Pareil pour les trois autres objets.
-    if ether_coord != ():
+    if laby.ether_coord != ():
         show_ether_coord = pygame.image.load(constants.IMAGE_ETHER).convert_alpha()
-        SCREEN.blit(show_ether_coord, ether_coord)
+        SCREEN.blit(show_ether_coord, laby.ether_coord)
 
-    if plastic_tube_coord != ():
+    if laby.plastic_tube_coord != ():
         show_plastic_tube_coord = pygame.image.load(constants.IMAGE_PLASTIC_TUBE).convert_alpha()
-        SCREEN.blit(show_plastic_tube_coord, plastic_tube_coord)
+        SCREEN.blit(show_plastic_tube_coord, laby.plastic_tube_coord)
 
-    if syringe_coord != ():
+    if laby.syringe_coord != ():
         show_syringe_coord = pygame.image.load(constants.IMAGE_SYRINGE).convert_alpha()
-        SCREEN.blit(show_syringe_coord, syringe_coord)
+        SCREEN.blit(show_syringe_coord, laby.syringe_coord)
 
     # Dimension prémier tuple est la position et le deuxiéme size. Affichage d'images des mûrs et des chemins.
-    for coord in list_walls_coord:
+    for coord in laby.list_walls_coord:
         SCREEN.blit(show_wall, coord)
-    for coord in list_floors_coord:
+    for coord in laby.list_floors_coord:
         SCREEN.blit(show_floor, coord)
 
 
-def end_game(list_walls_coord,
-            list_floors_coord,
-            position_guard_coord,
-            ether_coord,
-            plastic_tube_coord,
-            syringe_coord,
-            update_list_floors_coord,
-            heroe
-            ):
+def end_game(laby, heroe):
 
     # Fonction de conditions et d'affichage de fin de partie
     #print('show notification')
@@ -135,28 +97,10 @@ def end_game(list_walls_coord,
     pygame.display.update()
 
     # Appel de la fonction avec condition True pour la fermeture du jeux
-    init_game_structure(list_walls_coord,
-                        list_floors_coord,
-                        position_guard_coord,
-                        ether_coord,
-                        plastic_tube_coord,
-                        syringe_coord,
-                        update_list_floors_coord,
-                        heroe,
-                        True
-)
+    init_game_structure(laby, heroe, True)
 
 
-def manag_heroes_move(event,
-                      list_walls_coord,
-                      list_floors_coord,
-                      position_guard_coord,
-                      ether_coord,
-                      plastic_tube_coord,
-                      syringe_coord,
-                      update_list_floors_coord,
-                      heroe
-):
+def manag_heroes_move(event, laby, heroe):
     # Fonction de gestion des mouvements de l'heros et du rammassage d'objets
     # Définition initiales de coordonnées de gyver
     coord_hero_T1 = list(heroe.heroes_position)
@@ -172,51 +116,37 @@ def manag_heroes_move(event,
         # Modifier le tuple pour avoir la futur position
         coord_hero_T1[0] = coord_hero_T1[0] + 45
 
-    if tuple(coord_hero_T1) not in list_walls_coord:
+    if tuple(coord_hero_T1) not in laby.list_walls_coord:
         # fin de la partie je veux que gyver aille sur la case de guard
-        if tuple(coord_hero_T1) == position_guard_coord:
+        if tuple(coord_hero_T1) == laby.position_guard_coord:
             #Appelle de l'affichage de fin de partie
-            end_game(list_walls_coord,
-                     list_floors_coord,
-                     position_guard_coord,
-                     ether_coord,
-                     plastic_tube_coord,
-                     syringe_coord,
-                     update_list_floors_coord,
-                     heroe
-)
+            end_game(laby, heroe)
         # sinon si les coordonnées correspondent à ceux d'un objet alors
-        elif tuple(coord_hero_T1) == ether_coord:
+        elif tuple(coord_hero_T1) == laby.ether_coord:
             #on appel et modifie la liste des floors en lui enlévant la position de laby.ether_coord
-            update_list_floors_coord([ether_coord])
-            ether_coord = ()
+            laby.update_list_floors_coord([laby.ether_coord])
+            laby.ether_coord = ()
             #on rajoute +1 à l'inventaire de gyver
             #c'est la même methode pour les autres objets
             heroe.heroes_inventory += 1
-        elif tuple(coord_hero_T1) == syringe_coord:
-            update_list_floors_coord([syringe_coord])
-            syringe_coord = ()
+        elif tuple(coord_hero_T1) == laby.syringe_coord:
+            laby.update_list_floors_coord([laby.syringe_coord])
+            laby.syringe_coord = ()
             heroe.heroes_inventory += 1
-        elif tuple(coord_hero_T1) == plastic_tube_coord:
-            update_list_floors_coord([plastic_tube_coord])
-            plastic_tube_coord = ()
+        elif tuple(coord_hero_T1) == laby.plastic_tube_coord:
+            laby.update_list_floors_coord([laby.plastic_tube_coord])
+            laby.plastic_tube_coord = ()
             heroe.heroes_inventory += 1
 
         # On appel et modifie la fonction floors pour mettre à jours la position de l'heros
-        update_list_floors_coord([heroe.heroes_position, tuple(coord_hero_T1)])
+        laby.update_list_floors_coord([heroe.heroes_position, tuple(coord_hero_T1)])
         heroe.heroes_position = tuple(coord_hero_T1)
         # On appel la fonction pour actualiser l'affichage géneral du labyrinthe
-        set_case_definition(list_walls_coord,
-                            list_floors_coord,
-                            position_guard_coord,
-                            ether_coord,
-                            plastic_tube_coord,
-                            syringe_coord,
-                            heroe)
+        set_case_definition(laby, heroe)
 
 
 # # # # # # # # #
-#     MAIN      #
+#     MAIN
 # # # # # # # # #
 # Initialisation du Pygame
 pygame.init()
@@ -237,21 +167,13 @@ SCREEN.fill(constants.BACKGROUND_COLOUR)
 laby = Maze()
 heroe = Heroes(laby)
 
-# set_case_definition(laby, heroe)
+set_case_definition(laby, heroe)
 
 #Conditions génerales False pour la fin de partie. C'est à dire que gyver n'est pas encore arrivée vers le garde
 #et le jeux peut se derouler normalement
 
-    init_game_structure(list_walls_coord,
-                        list_floors_coord,
-                        position_guard_coord,
-                        ether_coord,
-                        plastic_tube_coord,
-                        syringe_coord,
-                        update_list_floors_coord,
-                        heroe,
-                        False
-                        )
+init_game_structure(laby, heroe, False)
+
 
 
 
