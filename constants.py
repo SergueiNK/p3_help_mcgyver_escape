e338f5a2c971204710616c09f5699d71caddc229
SergueiNK <nikolaev.serguei777@gmail.com>
2020-12-20 16:34:18 +0100

    all final modifications begin merge

--- constants.py
+++ constants.py
@@ -4,18 +4,18 @@ structure_file = 'res/structure.txt'
 tile_size = 45
 screen_size = (15 * tile_size, 15 * tile_size)
 
-"""The number of image displays per second"""
+# The number of image displays per second
 fps = 100
 
-"""RGB definition of color depth"""
+# RGB definition of color depth
 background_colour = (150, 150, 150)
 
-"""list of colors for texts"""
+# list of colors for texts
 
 green = (0, 255, 0)
 red = (255, 0, 0)
 
-"""list of used images"""
+# list of used images
 
 image_wall = "res/img/wall_tiles.png"
 image_floor = "res/img/floor_tiles.png"
